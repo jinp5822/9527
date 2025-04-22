ee59e3441085532321cb8d98634d79eaeeab9f79
jinp5822 <76906303+jinp5822@users.noreply.github.com>
2025-04-22 18:36:41 +0800

    更新 gonghui.js

--- gonghui.js
+++ gonghui.js
@@ -1,12 +1,18 @@
+
+/***
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+
 const url = `https://www.hfgh.org.cn/wxApi/v2/wxSignIn/signSave?usertoken=e4c3bd5f0e2245d2b598aac2dd27fbc1`;
 const method = `GET`;
 const headers = {
-    'Referer': `https://servicewechat.com/wx2086f2885316ec5a/84/page-frame.html`,
-    'Connection': `keep-alive`,
-    'Host': `www.hfgh.org.cn`,
-    'content-type': `application/json`,
-    'Accept-Encoding': `gzip,compress,br,deflate`,
-    'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.20(0x1800142f) NetType/4G Language/zh_CN`
+'Referer' : `https://servicewechat.com/wx2086f2885316ec5a/84/page-frame.html`,
+'Connection' : `keep-alive`,
+'Host' : `www.hfgh.org.cn`,
+'content-type' : `application/json`,
+'Accept-Encoding' : `gzip,compress,br,deflate`,
+'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.20(0x1800142f) NetType/4G Language/zh_CN`
 };
 const body = ``;
 
